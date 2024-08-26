c22867e6b53b2e342ecbe893f4720fd84bf90c61
Hayes Hundman <h.hundman@gmail.com>
2024-08-26 19:14:36 -0400

    button

--- frontend/src/lib/components/custom/search-command.svelte
+++ frontend/src/lib/components/custom/search-command.svelte
@@ -91,7 +91,7 @@
 </script>
 
 <p class="text-md pb-2 text-muted-foreground">
-	<button on:click={close_dialog}>
+	<button on:click={trigger_dialog}>
 		<kbd
 			class="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-foreground px-1.5 font-mono text-xs font-medium text-muted-foreground opacity-100"
 		>
